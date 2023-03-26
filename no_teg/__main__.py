caa39ddc4f8b534c02f9330415a743959e19b9a0
Aaron <aaron.ashery1@gmail.com>
2023-03-26 11:07:23 -0400

    Fixed linting issues

--- no_teg/__main__.py
+++ no_teg/__main__.py
@@ -1,4 +1,4 @@
 from .no_teg import example1
 
-if __name__ == "__main__": #pragma: no cover
-    example1() #pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
+    example1()  # pragma: no cover
